783f10259ab117af34040cd97bef8e94aa648d87
gitpractice <gitpractice@gmail.com>
2020-09-17 16:25:12 +0530

    adding arrays code

--- array.js
+++ array.js
@@ -26,3 +26,22 @@ person.mydetails.call(person, "svc", "vem"); //call method
 person.mydetails.apply(person, ["svu", "vem"]); //apply method parameters should give in arrays
 let a = person.mydetails.bind(person, "bee"); //it gives reference to new function we should call new function.
 a();
+console.log(Math.max.apply(null, [1, 3, 4]));
+console.log(Math.max.call(null, 1, 3, 4));
+
+let x = function() {
+  console.log("h");
+};
+x.call(this);
+
+var num1 = [23, 234, 34, 345, 56, 567];
+var num2 = [345, 45, 56];
+var num3 = [];
+var num4 = num3.concat(num1, num2);
+for (let i = 0; i < num4.length; i++) {
+  for (let j = i + 1; j < num4.length; j++) {
+    if (num4[i] === num4[j]) {
+      console.log(num4[i]);
+    }
+  }
+}
---
+++ class2.js
@@ -0,0 +1,111 @@
+// var cars = ["hyndai", "honda", "bmw"];
+
+// var bikes = new Array("herohonda", "tvs");
+// console.log(bikes);
+// //to string   (we get the o/p in commas)
+// console.log(cars.toString());
+// //push      push at last
+// cars.push("jagur");
+// console.log(cars);
+
+// //pop   last value will be deleted
+// console.log(cars.pop());
+
+// //unshift add at starting
+// cars.unshift("maruthi");
+// console.log(cars);
+// //shift     delete at starting
+// cars.shift();
+// console.log(cars);
+
+// //changing elements
+
+// cars[0] = "maruthi";
+// cars[cars.length] = "hyndai";
+
+// delete cars[1];
+// cars[1] = "jagur";
+
+// console.log(cars);
+
+// //splice to add elements
+
+// cars.splice(2, 0, "honda", "indica");
+// //delete one element
+// cars.splice(2, 1);
+// console.log(cars);
+// //merging
+// var vehicles = cars.concat(bikes);
+// console.log(vehicles);
+// //slicing
+
+// var scooters = vehicles.slice(5);
+// console.log(scooters);
+
+// var car = vehicles.slice(0, 5);
+// console.log(car);
+
+// //sorting for strings
+
+// //ascending
+// car.sort();
+// console.log(car);
+// //descending
+// car.reverse();
+// console.log(car);
+
+// //sorting for numbers
+
+// var numbers = [17, 1, 6, 23, 9, 2, 0];
+// numbers.sort();
+// console.log(numbers); //o/p: 0.17,2,23,6,9
+// //using function
+
+// numbers.sort(function(a, b) {
+//   return a - b; //ascending order
+// });
+// console.log(numbers);
+
+// //desecending order
+// numbers.sort(function(a, b) {
+//   return b - a;
+// });
+// console.log(numbers);
+// //finding elements
+
+// let number = [1, 2, 3, 4, 1];
+// console.log(number.indexOf(1));
+// console.log(number.lastIndexOf(1));
+// console.log(number.includes(1));
+
+//finding reference type elements
+const courses = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" }
+];
+
+//console.log(courses.includes({ id: 1, name: "a" }));
+const course = courses.findIndex(function(course) {
+  return course.id === 1;
+});
+console.log(course);
+
+// let courses = [
+//   { id: 1, name: "java" },
+//   { id: 2, name: "js" }
+// ];
+
+// let course = courses.find(function(course) {
+//   return course.name === "java";
+// });
+
+// console.log(course);
+
+// console.log("1");
+// setTimeout(function() {
+//   console.log("2");
+// }, 0);
+// console.log("3");
+
+
+var a=[{}]
